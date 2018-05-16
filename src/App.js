00e4bdc9381877540621b0ab1fe39b6bf89f4a74
James Boyer <jamesboyer@Jamess-MacBook-Pro.local>
2018-05-16 07:25:01 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactMapGL from 'react-map-gl';
+import './App.css';
+import '../node_modules/mapbox-gl/dist/mapbox-gl.css';
+ 
+class Map extends React.Component {
+ 
+  state = {
+    viewport: {
+      width: 600,
+      height: 700,
+      latitude: 39.7685,
+      longitude: -86.1580,
+      zoom: 13,
+      mapboxApiAccessToken: 'pk.eyJ1IjoiZ29ib3llciIsImEiOiJjamg4Z28xbm0wMXFnMzZ0ZHlqbHY1eHYyIn0.4GyOTtSo-i1WWPi1O8EYKg',
+      mapStyle: "mapbox://styles/mapbox/streets-v10",
+    }
+  };
+ 
+  render() {
+    return (
+      <div className="container">
+      <div className="camera"></div>
+      <ReactMapGL
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+      />
+      <div className="btn"></div>
+      </div>
+    );
+  }
+}
+
+export default Map;
+
