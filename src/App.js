574030f7ba6498b5c53e963639bd1aab02c10c14
James Boyer <jamesboyer@Jamess-MacBook-Pro.local>
2018-05-18 18:53:46 -0400

    Created seperate map component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import React from 'react';
-import ReactMapGL from 'react-map-gl';
+import Map from './Components/Map';
 import './App.css';
-import '../node_modules/mapbox-gl/dist/mapbox-gl.css';
  
-class Map extends React.Component {
- 
-  state = {
-    viewport: {
-      width: 600,
-      height: 700,
-      latitude: 39.7685,
-      longitude: -86.1580,
-      zoom: 13,
-      mapboxApiAccessToken: 'pk.eyJ1IjoiZ29ib3llciIsImEiOiJjamg4Z28xbm0wMXFnMzZ0ZHlqbHY1eHYyIn0.4GyOTtSo-i1WWPi1O8EYKg',
-      mapStyle: "mapbox://styles/mapbox/streets-v10",
-    }
-  };
+class App extends React.Component {
  
   render() {
     return (
+      
+      
 
       <div className="container">
 
     
       
         <div className="tablet">
+
         <div className="camera"></div>
-        <ReactMapGL
-          {...this.state.viewport}
-          onViewportChange={(viewport) => this.setState({viewport})}
-        />
+
+        <Map />
+        
         <div className="btn"></div>
+        
         </div>
 
         <div className="R_sidebox">
@@ -51,5 +41,5 @@ class Map extends React.Component {
   }
 }
 
-export default Map;
+export default App;
 
---
+++ src/Components/Map.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactMapGL from 'react-map-gl';
+import '../../node_modules/mapbox-gl/dist/mapbox-gl.css';
+
+class Map extends React.Component {
+ 
+    state = {
+      viewport: {
+        width: 600,
+        height: 700,
+        latitude: 39.7685,
+        longitude: -86.1580,
+        zoom: 13,
+        mapboxApiAccessToken: 'pk.eyJ1IjoiZ29ib3llciIsImEiOiJjamg4Z28xbm0wMXFnMzZ0ZHlqbHY1eHYyIn0.4GyOTtSo-i1WWPi1O8EYKg',
+        mapStyle: "mapbox://styles/mapbox/streets-v10",
+      }
+    };
+   
+    render() {
+      return (
+        <div>
+          <ReactMapGL
+            {...this.state.viewport}
+            onViewportChange={(viewport) => this.setState({viewport})}
+          />
+        </div>
+      );
+    }
+  }
+  
+  export default Map;
