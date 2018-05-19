0647324ecf1451ad564a0586434127e31a77a976
James Boyer <jamesboyer@Jamess-MacBook-Pro.local>
2018-05-19 16:18:31 -0400

    added footer component and style sheet

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,16 +2,27 @@ import React from 'react';
 import Map from './Map';
 import '../_Styles/Body.css';
 import R_Sidebox from './R_Sidebox';
+import Footer from './Footer';
  
 class Body extends React.Component {
  
   render() {
     return (
       <div className="Main">
+        
         <div className="Container"> 
-        <Map />
-          <R_Sidebox />          
+          
+          <div className="center">
+            <Map />
+            <R_Sidebox />
+           </div>
+
+            <div className="bottom">
+            <Footer />
+            </div>
+
         </div>
+        
       </div>
 
     );
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../_Styles/Footer.css';
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <div className="footer">
+                <h1>FOOTER</h1>
+            </div>
+        )
+    }
+}
+
+export default Footer;
